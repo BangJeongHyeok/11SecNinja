5404bbc51d47f4a9bb22d7369322fe81768df4f2
BangJeongHyeok <hyeok020514@naver.com>
2021-11-16 00:47:06 +0900

    슬로우 적용

--- Assets/Scripts/Character/CharacterControl.cs
+++ Assets/Scripts/Character/CharacterControl.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(LineRenderer))]
 public class CharacterControl : MonoBehaviour
 {
     //Component
     [SerializeField] Camera mainCamera;
     Rigidbody2D rigid;
+    LineRenderer lineRenderer;
 
     //Data
     Vector2 CurDir = Vector2.zero;
     [SerializeField] float MoveSpeed = 0;
     [SerializeField] float ReflectPower = 0;
 
+    //더블탭 딜레이
+    [SerializeField] float TouchDelay = 0;
+    float CurDelay = float.NegativeInfinity;
+    bool SlashTime = false;//썰어버리기
+    bool Slashed = false;//썰어버린 직후
+    Vector3 SlashOriginPos;//Slash 시작지점
+
 
 
     void ComponenetSetting()
     {
         rigid = GetComponent<Rigidbody2D>();
+        lineRenderer = GetComponent<LineRenderer>();
     }
 
     void Start()
@@ -30,22 +40,75 @@ public class CharacterControl : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        CharacterInput();
         ScreenColliderCheck();
+    }
+    private void Update()
+    {
+        CharacterInput();
+    }
+
+    void MouseDownAction()
+    {
+        if (Slashed)
+            Slashed = false;
+        else if (Time.time - CurDelay <= TouchDelay)
+        {
+            SlashTime = true;
+            SlashOriginPos = Input.mousePosition;
+            Vector3 FixedPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            lineRenderer.SetPosition(0, new Vector3(FixedPos.x, FixedPos.y, 0));
+        }
+    }
+
+    void MouseAction()
+    {
+        if (SlashTime)
+        {
+            Time.timeScale = Mathf.Lerp(Time.timeScale,0.2f,12f * Time.deltaTime);
+            Vector3 FixedPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            lineRenderer.SetPosition(1, new Vector3(FixedPos.x, FixedPos.y, 0));
+        }
+        else
+        {
+            CurDir = (mainCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+            rigid.AddForce(CurDir * MoveSpeed * Time.deltaTime);
+        }
+    }
+
+    void MouseUpAction()
+    {
+        if (SlashTime)//제드타임이면
+        {
+            Time.timeScale = 1f;
+            rigid.AddForce((Input.mousePosition - SlashOriginPos).normalized * 50, ForceMode2D.Impulse);
+
+            Vector3 FixedPos = mainCamera.ScreenToWorldPoint(SlashOriginPos);
+            lineRenderer.SetPosition(1, new Vector3(FixedPos.x, FixedPos.y, 0));
+            SlashTime = false;
+            Slashed = true;
+        }
+
+        CurDelay = Time.time;
     }
 
     void CharacterInput()
     {
+
+        if (Input.GetMouseButtonDown(0))
+            MouseDownAction();
+
         if (Input.GetMouseButton(0))//터치하고 있다면
-        {
-            CurDir = (mainCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
-            rigid.AddForce(CurDir * MoveSpeed);
-        }
+            MouseAction();
         else
             CurDir = Vector2.zero;
 
+        if (Input.GetMouseButtonUp(0))
+            MouseUpAction();
+
         transform.Rotate(rigid.velocity);
+        Debug.Log(SlashTime);
     }
+
 
     bool ScreenColliderCheck()
     {
