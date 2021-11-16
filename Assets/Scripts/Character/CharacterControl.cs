6899cb6bda78b26b274cf19e992a922028c9f817
BangJeongHyeok <hyeok020514@naver.com>
2021-11-16 18:59:51 +0900

    폰트 추가, 머테리얼 변경하는거 수정하느중

--- Assets/Scripts/Character/CharacterControl.cs
+++ Assets/Scripts/Character/CharacterControl.cs
@@ -15,7 +15,12 @@ public class CharacterControl : MonoBehaviour
     //Data
     Vector2 CurDir = Vector2.zero;
     [SerializeField] float MoveSpeed = 0;
+    [SerializeField] float SlashPower = 0;
     [SerializeField] float ReflectPower = 0;
+    [SerializeField] List<Color> BallColor;
+    Material BallMaterial;
+    Material TrailMaterial;
+    Color CurColor;
 
     //더블탭 딜레이
     [SerializeField] float TouchDelay = 0;
@@ -24,12 +29,16 @@ public class CharacterControl : MonoBehaviour
     bool Slashed = false;//썰어버린 직후
     Vector3 SlashOriginPos;//Slash 시작지점
 
-
+    IEnumerator ColorchangeIter;
 
     void ComponenetSetting()
     {
         rigid = GetComponent<Rigidbody2D>();
         lineRenderer = GetComponent<LineRenderer>();
+
+        BallMaterial = GetComponent<MeshRenderer>().materials[1];//OutLine
+        TrailMaterial = GetComponent<TrailRenderer>().materials[0];//Color
+        CurColor = BallMaterial.color;
     }
 
     void Start()
@@ -53,10 +62,13 @@ public class CharacterControl : MonoBehaviour
             Slashed = false;
         else if (Time.time - CurDelay <= TouchDelay)
         {
+            MaterialColorChange(1);
+
             SlashTime = true;
             SlashOriginPos = Input.mousePosition;
             Vector3 FixedPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
             lineRenderer.SetPosition(0, new Vector3(FixedPos.x, FixedPos.y, 0));
+
         }
     }
 
@@ -79,8 +91,10 @@ public class CharacterControl : MonoBehaviour
     {
         if (SlashTime)//제드타임이면
         {
+            MaterialColorChange(2);
+
             Time.timeScale = 1f;
-            rigid.AddForce((Input.mousePosition - SlashOriginPos).normalized * 50, ForceMode2D.Impulse);
+            rigid.AddForce((Input.mousePosition - SlashOriginPos).normalized * SlashPower, ForceMode2D.Impulse);
 
             Vector3 FixedPos = mainCamera.ScreenToWorldPoint(SlashOriginPos);
             lineRenderer.SetPosition(1, new Vector3(FixedPos.x, FixedPos.y, 0));
@@ -106,6 +120,13 @@ public class CharacterControl : MonoBehaviour
             MouseUpAction();
 
         transform.Rotate(rigid.velocity);
+
+        //슬래시 이후에 색 확인
+        if(!SlashTime)
+            if(!CheckColorClose(BallMaterial.color, BallColor[0]))
+                if(rigid.velocity.x < SlashPower || rigid.velocity.y < SlashPower)
+                    MaterialColorChange(0);
+
         Debug.Log(SlashTime);
     }
 
@@ -124,6 +145,34 @@ public class CharacterControl : MonoBehaviour
             rigid.AddForce((Vector3.zero - transform.position).normalized * ReflectPower, ForceMode2D.Impulse);
             return true;
         }
+        return false;
+    }
+
+    void MaterialColorChange(int ColorType)
+    {
+        StopAllCoroutines();
+        if (ColorchangeIter != null)
+            StopCoroutine(ColorchangeIter);
+
+        ColorchangeIter = ColorChange(ColorType);
+        StartCoroutine(ColorChange(ColorType));
+    }
+
+    IEnumerator ColorChange(int ColorType)
+    {
+        while(!CheckColorClose(BallMaterial.color, BallColor[ColorType]))
+        {
+            CurColor = Color.Lerp(CurColor, BallColor[ColorType], 15f * Time.deltaTime);
+            BallMaterial.color = CurColor;
+            TrailMaterial.SetColor("Color", CurColor * 0.8f);
+            yield return null;
+        }
+    }
+
+    bool CheckColorClose(Color color1, Color color2)
+    {
+        if(Mathf.Approximately(color1.r, color2.r) && Mathf.Approximately(color1.g, color2.g) && Mathf.Approximately(color1.b, color2.b))
+            return true;
         return false;
     }
 }
